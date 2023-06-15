6c0173aa6f4cc5db4485310ce413f9a73f7e2e63
unknown <rjpattnaik12345@gmail.com>
2023-06-16 00:22:57 +0530

    another commit

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from '../contextApi/UserContext';
+import axios from "axios";
 
 const AnimeCard = ({
   anime: { _id, title, synopsis, link, genres, image },
 }) => {
   const navigate=useNavigate();
-    const handleSubmit = (e) => {
+  const {user}=useContext(UserContext);
+  const history={
+    "userid":user._id,
+    "animeId":_id,
+    "title":title,
+    "image":image
+  }
+   
+    const handleSubmit = async(e) => {
       e.preventDefault();
-  
-        navigate(`/search/by_id/${_id}`);
+      try {
+          console.log(history);
+          await axios.post("http://localhost:4000/api/v1/history/new",history);
+          console.log("history is added")
+      } catch (error) {
+        console.log(error);
+      }
+       direct();
     };
+    const direct=()=>{
+      navigate(`/search/by_id/${_id}`);
+    }
   return (
     <div className="anime-card" key={_id}>
       <img src={image} alt="anime-img" />
